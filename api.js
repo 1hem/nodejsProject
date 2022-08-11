3aa1941e5c550354f581feb33e5018995f3c5f8b
hem <hemlaldulal.986@gmail.com>
2022-08-11 21:22:20 +0545

    Modified server port"

--- api.js
+++ api.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const productRouter = require('./Routes/products')
-const productData = require('./data/products.json')
+const express = require("express");
+const cors = require("cors");
+const productRouter = require("./Routes/products");
+const productData = require("./data/products.json");
 const app = express();
 
 //setup our server
 app.use(cors());
-app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', (req,res)=>{//passing product data into hbs file
-    res.render('index',{productData});
-})
+app.get("/", (req, res) => {
+  //passing product data into hbs file
+  res.render("index", { productData });
+});
 
 // app.get('/css/index.css', (req,res)=>{
 //     res.sendFile(__dirname+'/css/index.css');
@@ -20,9 +21,8 @@ app.get('/', (req,res)=>{//passing product data into hbs file
 //     res.send("WELCOME TO ECOMMERCE API")
 // })
 
-app.use('/api/products',productRouter)
+app.use("/api/products", productRouter);
 
-
-app.listen(4040,()=>{
-    console.log('SEREVER STARTED AT 4040');
-})
+app.listen(4040, () => {
+  console.log("SEREVER STARTED AT 4020");
+});
